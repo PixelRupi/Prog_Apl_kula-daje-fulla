017d5a8e86b57a7c87f0c68bf7ea94fd4f0b8dcd
PixelRupi <piotrekb.345@gmail.com>
2023-12-16 22:02:48 +0100

    Add files via upload

---
+++ kule-10/kule-10/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace kule_10
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel_scena = new System.Windows.Forms.Panel();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button_start = new System.Windows.Forms.Button();
+            this.button_add_kula = new System.Windows.Forms.Button();
+            this.button_sub_kula = new System.Windows.Forms.Button();
+            this.button_CHINA = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // panel_scena
+            // 
+            this.panel_scena.Location = new System.Drawing.Point(12, 12);
+            this.panel_scena.Name = "panel_scena";
+            this.panel_scena.Size = new System.Drawing.Size(776, 426);
+            this.panel_scena.TabIndex = 0;
+            // 
+            // button_start
+            // 
+            this.button_start.BackColor = System.Drawing.Color.Lime;
+            this.button_start.Location = new System.Drawing.Point(794, 12);
+            this.button_start.Name = "button_start";
+            this.button_start.Size = new System.Drawing.Size(102, 57);
+            this.button_start.TabIndex = 1;
+            this.button_start.Text = "START";
+            this.button_start.UseVisualStyleBackColor = false;
+            this.button_start.Click += new System.EventHandler(this.button_start_Click);
+            // 
+            // button_add_kula
+            // 
+            this.button_add_kula.Location = new System.Drawing.Point(794, 191);
+            this.button_add_kula.Name = "button_add_kula";
+            this.button_add_kula.Size = new System.Drawing.Size(102, 41);
+            this.button_add_kula.TabIndex = 0;
+            this.button_add_kula.Text = "Kula++";
+            this.button_add_kula.UseVisualStyleBackColor = true;
+            this.button_add_kula.Click += new System.EventHandler(this.button_add_kula_Click);
+            // 
+            // button_sub_kula
+            // 
+            this.button_sub_kula.Location = new System.Drawing.Point(794, 238);
+            this.button_sub_kula.Name = "button_sub_kula";
+            this.button_sub_kula.Size = new System.Drawing.Size(102, 41);
+            this.button_sub_kula.TabIndex = 4;
+            this.button_sub_kula.Text = "Kula--";
+            this.button_sub_kula.UseVisualStyleBackColor = true;
+            this.button_sub_kula.Click += new System.EventHandler(this.button_sub_kula_Click);
+            // 
+            // button_CHINA
+            // 
+            this.button_CHINA.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button_CHINA.BackgroundImage")));
+            this.button_CHINA.Location = new System.Drawing.Point(796, 338);
+            this.button_CHINA.Name = "button_CHINA";
+            this.button_CHINA.Size = new System.Drawing.Size(100, 100);
+            this.button_CHINA.TabIndex = 5;
+            this.button_CHINA.UseVisualStyleBackColor = true;
+            this.button_CHINA.Click += new System.EventHandler(this.button_CHINA_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(908, 450);
+            this.Controls.Add(this.button_CHINA);
+            this.Controls.Add(this.button_sub_kula);
+            this.Controls.Add(this.button_add_kula);
+            this.Controls.Add(this.button_start);
+            this.Controls.Add(this.panel_scena);
+            this.Name = "Form1";
+            this.Text = "Ballin\'";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel_scena;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button_start;
+        private System.Windows.Forms.Button button_add_kula;
+        private System.Windows.Forms.Button button_sub_kula;
+        private System.Windows.Forms.Button button_CHINA;
+    }
+}
+
---
+++ kule-10/kule-10/Form1.cs
@@ -0,0 +1,231 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Timers;
+using System.Numerics;
+using System.Security.Cryptography.X509Certificates;
+using System.Media;
+
+namespace kule_10
+{
+    public partial class Form1 : Form
+    {
+        public List<Kula> kulki = new List<Kula>();
+
+        //obiekt zegara
+        private System.Timers.Timer aTimer;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        public class Kula
+        {
+            Random randy = new Random();
+
+            //pozycja 
+            public float x;
+            public float y;
+            //promien
+            public float r;
+            //ograniczenie pola
+            public float x_min, x_max;
+            public float y_min, y_max;
+            // wektory
+            public int vec_x = 1;
+            public int vec_y = 1;
+
+            public string color = "red";
+
+            Panel scena;
+
+
+            // konsturktor
+            public Kula(Panel scena, float radius, float poz_x = 100f, float poz_y = 100f)
+            { 
+                this.scena = scena;
+                r = radius;
+                x = poz_x;
+                y = poz_y;
+            }
+
+            public Kula(Panel scena)
+            {
+                this.scena = scena;
+                r = (float)randy.Next(5, 25);
+                x = (float)randy.Next((int)r, scena.Width - (int)r);
+                y = (float)randy.Next((int)r, scena.Height - (int)r);
+            }
+
+            public void przesunObiekt(List<Kula> kulases)
+            {
+                float odleglosc;
+                x += (float)vec_x;
+                y += (float)vec_y;
+                Console.WriteLine("kula: x: " + x + " y: " + y);
+
+                if (x <= r)
+                {
+                    vec_x *= -1;
+                } else if (x >= (scena.Width - r))
+                {
+                    vec_x *= -1;
+                }
+
+                if (y <= r)
+                {
+                    vec_y *= -1;
+                } else if (y >= (scena.Height - r))
+                {
+                    vec_y *= -1;
+                }
+
+
+                foreach (Kula kula in kulases)
+                {
+                    if (kula != this)
+                    {
+                        if (Math.Sqrt(Math.Pow(kula.x - x, 2)+Math.Pow(kula.y - y, 2)) <= kula.r + r)
+                        {
+                            vec_x *= -1;
+                            vec_y *= -1;
+                        }
+                    }
+                }
+
+            }
+
+
+            public void rysujKule()
+            {
+                Graphics g = scena.CreateGraphics();
+
+                Pen pen = new Pen(Color.Black, 5);
+                pen.Color = Color.FromName(color);
+
+                g.DrawEllipse(pen, x - r, y - r, r + r, r + r);
+            }
+
+        }
+
+        /**
+        Kula kula01 = new Kula()
+        {
+            x = 750f,
+            y = 380f,
+            r = 20f,
+            color = "blue"
+        };
+        */
+        
+
+        
+        public void czyszczenieEkranu()
+        {
+            SolidBrush filler_brush = new SolidBrush(Color.White);
+            Rectangle clearing = new Rectangle(0, 0, panel_scena.Width, panel_scena.Height);
+            Graphics g = panel_scena.CreateGraphics();
+            g.FillRectangle(filler_brush, clearing);
+        }
+        
+        
+        /**
+        public void rysujKule()
+        {
+            //rysowanie pierwszej kuli:
+            Graphics g = panel1.CreateGraphics();
+
+            Pen pen = new Pen(Color.Black, 5);
+            pen.Color = Color.FromName(kula01.color);
+
+            czyszczenieEkranu();
+            g.DrawEllipse(pen, kula01.x - kula01.r, kula01.y - kula01.r, kula01.r+ kula01.r, kula01.r + kula01.r);
+            kula01.przesunObiekt();
+
+
+            //sprawdzanie, czy nie trzeba odwrocic wektora
+            if (kula01.x <= (kula01.r))
+            {
+                kula01.vec_x *= -1;
+            } else if (kula01.x >= (panel1.Width - kula01.r))
+            {
+                kula01.vec_x *= -1;
+            }
+            
+            if (kula01.y <= (kula01.r))
+            {
+                kula01.vec_y *= -1;
+            } else if (kula01.y >= (panel1.Height - kula01.r))
+            {
+                kula01.vec_y *= -1;
+            } 
+        }
+        **/
+
+
+        public  void OnTimedEvent(Object source, ElapsedEventArgs e)
+        {
+            Console.WriteLine("The Elapsed event was raised at {0:HH:mm:ss.fff}",e.SignalTime);
+            //kurwa
+
+            czyszczenieEkranu();
+            foreach (Kula k in kulki)
+            {
+                k.rysujKule();
+                k.przesunObiekt(kulki);
+            }
+        }
+
+
+        public void button_start_Click(object sender, EventArgs e)
+        {
+            SetTimer();
+            //kula01.rysujKule();
+            //rysujKule();
+        }
+
+        private void button_stop_Click(object sender, EventArgs e)
+        {
+            aTimer.Stop();
+        }
+        private void SetTimer()
+        {
+            // Create a timer with a two second interval.
+            aTimer = new System.Timers.Timer(2);
+            // Hook up the Elapsed event for the timer. 
+            aTimer.Elapsed += OnTimedEvent;
+            aTimer.AutoReset = true;
+            aTimer.Enabled = true;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            aTimer.Stop();
+            kulki.Clear();
+        }
+
+        private void button_add_kula_Click(object sender, EventArgs e)
+        {
+            kulki.Add(new Kula(panel_scena));
+        }
+
+        private void button_sub_kula_Click(object sender, EventArgs e)
+        {
+            kulki.RemoveAt(kulki.Count - 1);
+        }
+
+        private void button_CHINA_Click(object sender, EventArgs e)
+        {
+            SoundPlayer music = new SoundPlayer();
+            music.SoundLocation = "music.wav";
+            music.PlayLooping();
+        }
+    }
+}
